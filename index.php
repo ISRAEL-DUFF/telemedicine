c566785e3c415186e227b9a0fcdee1440f94a8d1
= <=>
2021-09-26 09:48:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+					<?php
+					include("header.php");
+					include("find.php");
+					?>
+<body onload="initialize()"><!--banner-section-->
+<div class="container-fluid">
+    <div class="banner">
+        <div class="container">
+            <div class="col-lg-12">
+                <div class="banner-text animated fadeInLeft">
+                    <h4>EKENG-ITA OFFIONG EKENG's</h4>
+                    <h2>Telemedicine App</h2>
+                    <p>
+                    A TELECONSULTATION APPLICATION FOR SUPPORTING DOCTOR-PATIENT COMMUNICATION
+                    </p>
+                    
+                    <div class="scroll">
+                        <img class="hvr-grow" src="assets/images/home/scroll.png" alt=""/>
+                        <h3 >Scroll Down</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--banner-section-->
+<!--map-section-->
+<div class="container-fluid">
+    <div class="map">
+        <div class="man">
+            <img src="assets/images/home/man.png" class="img-responsive animated fadeInDown" alt=""/>
+        </div>
+        <div class="container">
+            <div class="map-text">
+                <h3>HEALTHCARE AT YOUR DEMAND !</h3>
+                <h4>Find a nearby doctor or dentist and book an appointment instantly. And it's free !</span></h4>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php include("footer.php"); ?>
